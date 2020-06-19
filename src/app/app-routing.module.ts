d9783389edc3544e476d337bf0687f61dbc03ba3
Dmitry Weiner <dmitry.weiner@gmail.com>
2020-06-19 16:53:10 +0700

    Set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ChatViewComponent} from './chat/chat-view/chat-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+  { path: 'chat', component: ChatViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
